02b45532d5d0ce799e1940823741c017cc5a9a7c
Timothy RYAN <ryaninlux@gmail.com>
2022-10-26 16:36:24 -0700

    finished project. Start to test and create reducers

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import ticketsImage from "./../img/ticket.jpg";
 
 function Header(){
   return (
-    <ReactFragment>
+    <React.Fragment>
       <h1>Help Queue</h1>
       <img src={ticketsImage} alt="Image of a ticket" />
-    </ReactFragment>
+    </React.Fragment>
   );
 }
 
